d52061be5e6000b7651b54ae36884ccfc227eb60
yaronyadid <48672103+yaronyadid@users.noreply.github.com>
2020-03-18 21:14:43 +0200

    Add files via upload

---
+++ c_lab_ex1/c_lab_ex1/parser.h
@@ -0,0 +1,17 @@
+#ifndef PARSER_HEADER
+#define PARSER_HEADER
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#define ADD_END 1
+#define ADD_START 2
+#define ADD_AFTER 3
+#define FIND_INDEX 4
+#define DELETE_INDEX 5
+#define PRINT 6
+#define EXIT 7
+#define INVALID 8
+
+#endif 
